73eb4099e34a2b09b1e40548ddb303ce81ee58ee
Birkeholm <mikkel.birkeholm@gmail.com>
2023-09-18 14:16:59 +0200

    Call To Action
    OBS: Har valgt at differentiere mellem Find Station-knap og login, da det er to meget forskellige formål

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,24 @@
 import { Slideshow } from '@/components/Slideshow/Slideshow'
+import Link from 'next/link'
 
 export default async function Home() {
   return (
     <main>
       <Slideshow />
+      <div className="cta">
+        <h2>Find og anmeld genbrugsstationer</h2>
+        <div>
+          <Link href="#">
+            <button>Find station</button>
+          </Link>
+          <Link
+            href="#"
+            className="alt-btn"
+          >
+            Log Ind
+          </Link>
+        </div>
+      </div>
       <div className="contentwrapper">
         <h1>Boilerplate</h1>
         <hr />
